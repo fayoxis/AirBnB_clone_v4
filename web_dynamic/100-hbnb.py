f40906109214d84ed76d4393308cf0ec63e5a0d4
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-02 22:20:41 +0100

    Update 100-hbnb.py

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""The 100-hbnb module
-"""
+"""The HBNB (HBnB) Flask Application"""
+
 from models import storage
 from models.state import State
 from models.city import City
@@ -9,40 +9,30 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
-app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
+app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """Close the current database session"""
     storage.close()
-
 
 @app.route('/100-hbnb/', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
+    """Render the HBNB template"""
+    states = sorted(storage.all(State).values(), key=lambda k: k.name)
+    state_cities = [[state, sorted(state.cities, key=lambda k: k.name)] for state in states]
+    amenities = sorted(storage.all(Amenity).values(), key=lambda k: k.name)
+    places = sorted(storage.all(Place).values(), key=lambda k: k.name)
 
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+    return render_template(
+        '100-hbnb.html',
+        states=state_cities,
+        amenities=amenities,
+        places=places,
+        cache_id=uuid.uuid4()
+    )
 
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    return render_template('100-hbnb.html',
-                           states=st_ct,
-                           amenities=amenities,
-                           places=places,
-                           cache_id=uuid.uuid4())
-
-
-if __name__ == "__main__":
-    """ Main Function """
+while __name__ == "__main__":
+    """Run the Flask application"""
     app.run(host='0.0.0.0', port=5001)
