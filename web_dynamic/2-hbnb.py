2dca50cf8816137082192f6089aec17f1ea4f419
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-02 22:13:32 +0100

    Update 2-hbnb.py

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -1,30 +1,39 @@
-#!/usr/bin/python3
-"""The 2-hbnb module
-"""
-from uuid import uuid4
+#!/usr/bin/env python3
+"""Flask Web Application for HBNB"""
 
-from flask import Flask, render_template
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+import os
+import uuid
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.teardown_appcontext
+def close_db(error):
+    """Close the current SQLAlchemy Session"""
+    storage.close()
 
 @app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
-    """Displays the HBNB main page
-    """
-    states = storage.all('State').values()
-    amenities = storage.all('Amenity').values()
-    places = storage.all('Place').values()
-    return render_template('2-hbnb.html', states=states, amenities=amenities,
-                           places=places, cache_id=uuid4())
+    """Render the HBNB template"""
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    state_cities = [[state, sorted(state.cities, key=lambda city: city.name)] for state in states]
 
+    amenities = sorted(storage.all(Amenity).values(), key=lambda amenity: amenity.name)
+    places = sorted(storage.all(Place).values(), key=lambda place: place.name)
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
-    storage.close()
+    return render_template(
+        '2-hbnb.html',
+        state_cities=state_cities,
+        amenities=amenities,
+        places=places,
+        cache_id=uuid.uuid4()
+    )
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    """Start the Flask app"""
+    app.run(host='0.0.0.0', port=5000)
