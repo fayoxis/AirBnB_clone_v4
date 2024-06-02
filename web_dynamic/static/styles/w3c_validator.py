9330bb6e50239d28cc64435ba7b56935b0fc7d19
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-02 23:01:42 +0100

    Update w3c_validator.py

--- web_dynamic/static/styles/w3c_validator.py
+++ web_dynamic/static/styles/w3c_validator.py
@@ -1,37 +1,6 @@
 #!/usr/bin/python3
 """
-W3C validator for Holberton School
-
-For HTML and CSS files.
-
-Based on 2 APIs:
-
-- https://validator.w3.org/nu/
-- http://jigsaw.w3.org/css-validator/validator
-
-
-Usage:
-
-Simple file:
-
-```
-./w3c_validator.py index.html
-```
-
-Multiple files:
-
-```
-./w3c_validator.py index.html header.html styles/common.css
-```
-
-All errors are printed in `STDERR`
-
-Return:
-Exit status is the # of errors, 0 on Success
-
-References
-
-https://developer.mozilla.org/en-US/
+Validate HTML and CSS files using W3C APIs
 
 """
 import sys
