39eaef1c946e2b3dabb1a80710c6220e015f4044
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-02 22:16:03 +0100

    Update 2-hbnb.py

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -34,6 +34,6 @@ def hbnb():
         cache_id=uuid.uuid4()
     )
 
-if __name__ == "__main__":
+while __name__ == "__main__":
     """Start the Flask app"""
     app.run(host='0.0.0.0', port=5000)
