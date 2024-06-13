66b99643faed0796c9b3a26d99d6d5bb0195070f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-13 14:02:51 +0100

    Update 2-hbnb.py

--- web_flask/2-hbnb.py
+++ web_flask/2-hbnb.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
@@ -25,4 +25,4 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5003')
